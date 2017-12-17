26319a35aa7cdf395fab78f78f45c5cd3b3964b7
Sebastien Laithier <sebastien.laithier@smile.fr>
2017-12-18 00:27:15 +0100

    Adding admin interface

--- collectionHandler/videogameshelf/admin.py
+++ collectionHandler/videogameshelf/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import videoGame
+from .models import editor
+
+admin.site.register(videoGame)
+admin.site.register(editor)
--- collectionHandler/videogameshelf/models.py
+++ collectionHandler/videogameshelf/models.py
@@ -3,11 +3,15 @@ from django.db import models
 # Create your models here.
 
 class editor(models.Model):
+    def __str__(self):
+        return self.name
     name = models.CharField(max_length=200)
     country = models.CharField(max_length=200)
     creationDate= models.IntegerField(default=0)
 
 class videoGame(models.Model):
+    def __str__(self):
+        return self.title
     title = models.CharField(max_length=200)
     editor = models.ForeignKey(editor,on_delete=models.PROTECT)
     platform = models.CharField(max_length=200)
