c0b2a52787631f278e803497a387880e70baf8ee
kirby0025 <kirby@portable.hyrule>
2017-12-18 01:32:03 +0100

    Adding platform and manufacturer models

--- collectionHandler/videogameshelf/admin.py
+++ collectionHandler/videogameshelf/admin.py
@@ -4,6 +4,10 @@ from django.contrib import admin
 
 from .models import videoGame
 from .models import editor
+from .models import manufacturer
+from .models import platform
 
 admin.site.register(videoGame)
 admin.site.register(editor)
+admin.site.register(manufacturer)
+admin.site.register(platform)
--- collectionHandler/videogameshelf/models.py
+++ collectionHandler/videogameshelf/models.py
@@ -6,14 +6,40 @@ class editor(models.Model):
     def __str__(self):
         return self.name
     name = models.CharField(max_length=200)
-    country = models.CharField(max_length=200)
+    country = models.CharField(max_length=50)
     creationDate= models.IntegerField(default=0)
+
+class manufacturer(models.Model):
+    def __str__(self):
+        return self.name
+    name = models.CharField(max_length=200)
+    country = models.CharField(max_length=50)
+    creationDate = models.IntegerField(default=0)
+
+class platform(models.Model):
+    def __str__(self):
+        return self.name
+    PC = 'PC'
+    SALON = 'SA'
+    PORTABLE = 'PO'
+    HYBRIDE = 'HY'
+    PLATFORM_TYPE = (
+        (PC, 'PC'),
+        (SALON, 'Salon'),
+        (PORTABLE, 'Portable'),
+        (HYBRIDE,'Hybride'),
+    )
+    name  = models.CharField(max_length=100)
+    category = models.CharField(max_length=10,choices=PLATFORM_TYPE,default=PC)
+    year = models.IntegerField(default=0)
+    constructor = models.ForeignKey(manufacturer,on_delete=models.PROTECT)
 
 class videoGame(models.Model):
     def __str__(self):
         return self.title
     title = models.CharField(max_length=200)
     editor = models.ForeignKey(editor,on_delete=models.PROTECT)
-    platform = models.CharField(max_length=200)
+    platform = models.ForeignKey(platform,on_delete=models.PROTECT)
+    year = models.IntegerField(default=0)
     played = models.BooleanField(default=False)
     finished = models.BooleanField(default=False)
